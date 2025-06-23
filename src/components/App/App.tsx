8e7dc4ceb4430822dc858cf59de932fe4ec6edff
unknown <do22lytvynenko@gmail.com>
2025-06-23 14:12:05 +0300

    hww

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,50 +1,53 @@
 import { useState } from 'react';
 import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
-import type {Votes, VoteType} from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
+import { type Votes } from "../../types/votes";
+import { type VoteType } from "../../types/votes";
+ 
 
+function App() {
 
-export default function App() {
-  const [votes, setVotes] = useState<Votes>({
+  const initialState: Votes = {
     good: 0,
     neutral: 0,
-    bad: 0,
-  });
+    bad: 0
+}
+const [votes, setVotes] = useState<Votes>(initialState);
 
 
-  const handleVote = (type: VoteType) => {
-    setVotes(prevVotes => ({
-      ...prevVotes,
-      [type]: prevVotes[type] + 1,
-    }));
-  };
-  const resetVotes = () => {
+
+const handleVote = (vote: VoteType) => {
     setVotes({
-      good: 0,
-      neutral: 0,
-      bad: 0,
+        ...votes,
+        [vote]: votes[vote] + 1,
     });
-  };
-  const totalVotes = votes.good + votes.neutral + votes.bad;
-  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
-
-  return (
-  
-      <div className={css.app}>
-        <CafeInfo/>
-          <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0 }/>
-            {totalVotes > 0 ? (
-              <VoteStats votes= {votes} totalVotes={totalVotes} positiveRate={positiveRate}/>
-            ) : (
-             <Notification/>
-              )
-            }
-      </div>
-
-    
-  );
 }
 
+const resetVotes = () => {
+    setVotes(initialState);
+}
+
+const totalVotes = votes.good + votes.neutral + votes.bad
+ 
+const canReset = Boolean(totalVotes)  
+  
+const positiveRate = totalVotes
+? Math.round((votes.good / totalVotes) * 100)
+: 0
+
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={canReset} />
+      { totalVotes > 0 ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />
+      : <Notification />}
+    </div>
+
+  )
+}
+
+export default App
